f97cbf221c9054ae73baeeab45e60d6ee062b824
Gyu-Ho Lee <gyuho.cs@gmail.com>
2014-03-07 09:08:49 -0800

    Update Package Description

--- gotree.go
+++ gotree.go
@@ -1,8 +1,8 @@
-// gotree provides tree visualizing tools and tree algorithm implementations.
+// gotree provides tree visualizing tools and algorithm implementations.
 package main
 
 import "fmt"
 
 func main() {
-	fmt.Println("Gotree provides tree visualizing tools and tree algorithm implementations.")
+	fmt.Println("Gotree provides tree visualizing tools and algorithm implementations.")
 }
