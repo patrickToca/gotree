81806188b3c20fb12c91a609164c3beaf19d2391
Gyu-Ho Lee <gyuho.cs@gmail.com>
2014-03-20 23:48:07 -0700

    Update

--- example/bst_viz_test.go
+++ example/bst_viz_test.go
@@ -6,7 +6,6 @@ import (
 	// go test -v github.com/gyuho/gotree/example
 	// go test -v /Users/gyuho/go/src/github.com/gyuho/gotree/example/bst_viz_test.go
 	"github.com/gyuho/gotree/tree/bst"
-	"github.com/gyuho/gotree/tree/bst/viz"
 )
 
 func Test_bst_viz(test *testing.T) {
@@ -18,5 +17,5 @@ func Test_bst_viz(test *testing.T) {
 		t1 = t1.Insert(int64(i))
 	}
 	fmt.Println(t1.Find(6)) // &{<nil> 6 0x210323140 1}
-	viz.Show(t1, "t1.dot")
+	// viz.Show(t1, "t1.dot")
 }
